0e5ffbe27e7044b0feaf0f34a7a452489adadc0a
Brett Jackson <brettbj.jackson@gmail.com>
2012-11-06 02:28:46 -0600

    initial commit

---
+++ finaldesign/finaldesign.ino
@@ -0,0 +1,26 @@
+
+// ECE 110 Lab
+// Final Design
+// (c) Brett Jackson. All Rights Reserved.
+// bbjacks2@illinois.edu
+
+// Variable declarations
+
+// Pin declarations
+
+
+void setup() {
+  
+}
+
+void loop() {
+  
+}
+
+void tape_follow() {
+  
+}
+
+void tape_avoid() {
+  
+}
