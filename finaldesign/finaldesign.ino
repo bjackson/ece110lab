316bc06074bc42f4b74cdc70d0177be0d2ab12a7
Brett Jackson <brettbj.jackson@gmail.com>
2012-11-08 01:07:50 -0600

    1am commit

--- finaldesign/finaldesign.ino
+++ finaldesign/finaldesign.ino
@@ -1,26 +1,97 @@
-
 // ECE 110 Lab
 // Final Design
-// (c) Brett Jackson. All Rights Reserved.
+// Brett Jackson
 // bbjacks2@illinois.edu
+// Copyright (c) 2012 Brett Jackson
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without
+// limitation the rights to use, copy, modify, merge, publish, distribute,
+// sublicense, and/or sell copies of the Software, and to permit persons
+// to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+// THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#include <SoftwareSerial.h>
 
 // Variable declarations
+int rightMotorSpeed          = 0;
+int leftMotorSpeed           = 0;
+int colorDetected            = 0;
+int splitDetected            = 0;
+int leftTapeDetected         = 0;
+int rightTapeDetected        = 0;
+int leftSplitDetected        = 0;
+int rightSplitDetected       = 0;
+
+
 
 // Pin declarations
+const int rightMotor         = 14; // connect this pin to right CA module, pin A0
+const int leftMotor          = 15; // connect this pin to left  CA module, pin A1
+const int rightTapeAvoiding  = 2;
+const int leftTapeAvoiding   = 3;
+const int rightSplitDetector = 4;
+const int leftSplitDetector  = 5;
+const int colorDetector      = 6;
+
+
 
 
 void setup() {
-  
+  Serial.begin(9600);
 }
 
 void loop() {
+  leftSplitDetected  = digitalRead(leftSplitDetector);
+  rightSplitDetected = digitalRead(rightSplitDetector);
+  colorDetected      = digitalRead(colorDetector);
+  leftTapeDetected   = digitalRead(leftTapeAvoiding);
+  rightTapeDetected  = digitalRead(rightTapeAvoiding);
+  
+  if (leftSplitDetected == 1 && rightSplitDetected == 1) {
+    splitDetected = 1;
+    Serial.println("Split detected.");
+  } else {
+    splitDetected = 0;
+  }
+  
+  
+  if (splitDetected == 1) {
+    if (colorDetected == 1) {
+      colorDetectedRoutine();
+      Serial.println("Split with color detected.");
+    }
+   } else { 
+      splitDetectedRoutine(); 
+    }
+
   
 }
 
 void tape_follow() {
-  
+  Serial.println("Following tape.");
 }
 
 void tape_avoid() {
-  
+  Serial.println("Avoiding tape.");
+}
+
+void colorDetectedRoutine() {
+  Serial.println("ColorDetectedRoutine started.");
+}
+
+void splitDetectedRoutine() {
+  Serial.println("SplitDetectedRoutine started.");
 }
